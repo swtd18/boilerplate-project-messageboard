a0f2ebb87349e3841a9c5fafb3cda7b0e1bb7627
SWTD 18E <pseude18@gmail.com>
2023-03-06 10:39:24 -0700

    sdsdsd

--- models.js
+++ models.js
@@ -1,15 +1,35 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
-
-const MessageSchema = new Schema({
-    _id: { type: Number, required: true },
-    text: { type: String, default: "" },
-    created_on: { type: String, default: ""},
-    bumped_on : { type: String, default: "" },
+const date=new Date();
+const ReplySchema = new Schema({
+    text: { type: String},
+    created_on: { type: Date, default: date},
+    bumped_on : { type: Date, default: date },
     reported :{ type: Boolean, default: false },
-    delete_password:{ type: String, default: "" },
-    replies : { type: String, default: [] }
+    delete_password:{ type: String},
 });
-const Message = mongoose.model("Message", MessageSchema);
 
-exports.Message = Message;
+const reply = mongoose.model("Reply", ReplySchema);
+
+const ThreadSchema = new Schema({
+    text: { type: String, default: "" },
+    created_on: { type: Date, default: date},
+    bumped_on : { type: Date, default: date },
+    reported :{ type: Boolean, default: false },
+    delete_password:{ type: String},
+    replies : { type: [ReplySchema]}
+});
+
+const thread = mongoose.model("Thread", ThreadSchema);
+
+const BoardSchema = new Schema({
+    name:{ type: String},
+    threads: { type: [ThreadSchema]},
+});
+
+const board = mongoose.model("Board", BoardSchema);
+
+
+exports.Board = board;
+exports.Reply = reply;
+exports.Thread = thread;
--- routes/api.js
+++ routes/api.js
@@ -1,36 +1,85 @@
 'use strict';
-const MessageModel = require("../models").Message;
+const BoardModel = require("../models").Board;
+const ThreadModel = require("../models").Thread;
+const RaplyModel = require("../models").Reply;
 
-function saveMessage(text,delete_password) {
-
-  const newMessage = new MessageModel({
-    _id: Math.floor((Math.random() * 100) + 1),
-    text: text,
-    created_on:String(new Date().getTime()),
-    bumped_on : String(new Date().getTime()),
-    reported :false,
-    delete_password:delete_password,
-    replies :""
-  });
-  const savedNew = newMessage.save();
-}
-
-async function findMessage(text) {
-  return await MessageModel.findOne({ text: text }).exec();
-}
 
 
 module.exports = function (app) {
   
-  app.route('/api/threads/:board').post(async function (req,res) {
+  app.route('/api/threads/:board').post(function (req,res) {
     const {text, delete_password}=req.body;
-    console.log({text,delete_password});
-    //saveMessage(text,delete_password);
+    let board=req.body.board
+    if(!board) {
+      board=req.params.board;
+    }
+    const newThread=new ThreadModel({
+      text:text,
+      delete_password:delete_password,
+      replys:[]
+    })
+    BoardModel.findOne({name:board}).then(data=> {
+      if(!data) {
+        const newBoard=new BoardModel({
+          name:board,
+          threads:[newThread]
+        })
+        newBoard.save().then(data=> {
+          if(!data) {
+            console.log("error saving data");
+            res.send("there was error in saving data");
+          }
+          else {
+            res.json(newThread);
+          }
+        })
+      }
+      else {
+        BoardModel.threads.push(newThread);
+        newBoard.save((err,data)=> {
+          if(!data) {
+            console.log("error saving data");
+            res.send("there was error in saving data");
+          }
+          else {
+            res.json(newThread);
+          }
+        })
+      }
+    })  
 
-    const findResult = findMessage("dqd");
-    console.log(findResult);
+  }).get((req,res)=> {
+    const board=req.params.body;
+    console.log(board);
+    BoardModel.findOne({name:board}).then(data=> {
+     if(!data) {
+       console.log("this thread does not exist"); 
+     }
+     else {
+       const threads=data.threads.map(thread=> {
+       const {
+        _id,
+        text,
+        created_ond,
+        bumped_on,
+        delete_password,
+        replies
+        }=thread;
+        return {
+          _id,
+          text,
+          created_ond,
+          bumped_on,
+          delete_password,
+          replies,
+          replyCount:thread.replies.length,
+        };
+       })
+       res.json(threads);
+     }
+    })
 
-  });
+  })
  
   app.route('/api/replies/:board');
 }
