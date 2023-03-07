3cba63b9408aa9f17cc2513af8242f2c635e80e2
swtd18 <126660158+swtd18@users.noreply.github.com>
2023-03-07 15:39:15 -0700

    dqd

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,7 @@
 'use strict';
 const BoardModel = require("../models").Board;
 const ThreadModel = require("../models").Thread;
-const RaplyModel = require("../models").Reply;
+const ReplyModel = require("../models").Reply;
 
 
 
@@ -9,12 +9,15 @@ module.exports = function (app) {
 
   app.route('/api/threads/:board').post(function (req,res) {
     const {text, delete_password}=req.body;
+    const date=new Date();
     let board=req.body.board
     if(!board) {
       board=req.params.board;
     }
     const newThread=new ThreadModel({
       text:text,
+      created_on:date,
+      bumped_on:date,
       delete_password:delete_password,
       replys:[]
     })
@@ -60,16 +63,15 @@ module.exports = function (app) {
        const {
         _id,
         text,
-        created_ond,
+        created_on,
         bumped_on,
-
         delete_password,
         replies
         }=thread;
         return {
           _id,
           text,
-          created_ond,
+          created_on,
           bumped_on,
           delete_password,
           replies,
@@ -79,8 +81,162 @@ module.exports = function (app) {
        res.json(threads);
      }
     })
+    
 
-  })
+  }).delete((req,res)=>{
+    const{thread_id,delete_password}=req.body;
+    let board=req.body.board;
+    if(!board) {
+      board=req.params.board;
+    }
+    BoardModel.findOne({name:board}).then((data)=>{
+      if(!data) {
+        console.log("board is not found");
+      }
+      else {
+        let threadToDelete=data.threads.id(thread_id);
+        if(threadToDelete.delete_password===delete_password) {
+          threadToDelete.deleteOne();
+          data.save().then((data)=>{
+            if(!data) {
+              console.log("error updating in database");
+            }
+            else {
+              res.send("success");
+            }
+          });
+        }
+        else {
+          res.send("incorrect password");
+        }
+      }
+    });
+  }).put((req,res)=>{
+    const{thread_id}=req.body;
+    let board=req.body.board;
+    if(!board) {
+      board=req.params.board;
+    }
+
+    BoardModel.findOne({name:board}).then((data)=>{
+      if(!data) {
+        console.log("board is not found");
+      }
+      else {
+        let threadReported=data.threads.id(thread_id);
+        threadReported.reported=true;
+        threadReported.bumped_on=new Date();
+        data.save().then((data)=>{
+            if(!data) {
+              console.log("error updating in database");
+            }
+            else {
+              res.send("reported");
+            }
+          });
+        }  
+    });
+  });
  
-  app.route('/api/replies/:board');
+  app.route('/api/replies/:board').post((req,res)=>{
+    const {text,delete_password,thread_id}=req.body;
+    const date=new Date();
+    let board=req.body.board
+    if(!board) {
+      board=req.params.board;
+    }
+    const newReply=new ReplyModel({
+      text:text,
+      created_on:date,
+      delete_password:delete_password
+    });
+    BoardModel.findOne({name:board}).then((data)=>{
+      if(!data) {
+        console.log('this board is not found');
+      }
+      else {
+        const foundthread=data.threads.id(thread_id);
+         foundthread.bumped_on=date;
+         foundthread.replies.push(newReply);
+         data.save().then((data)=>{
+          if(!data){
+            console.log("the data couldn't be saved");
+          }
+          else {
+            res.json(foundthread);
+          }
+         })
+         }
+        })
+      })
+      .get((req,res)=>{
+        let board=req.body.board
+        if(!board) {
+          board=req.params.board;
+        }
+        BoardModel.findOne({name:board}).then((data)=>{
+          if(!data) {
+            console.log('this board is not found');
+          }
+          else {
+            const thread=data.threads.id(req.query.thread_id);
+            res.json(thread);
+             }
+         })
+      }).put((req,res)=>{
+        const{thread_id,reply_id}=req.body;
+        let board=req.body.board;
+        if(!board) {
+          board=req.params.board;
+        }
+        BoardModel.findOne({name:board}).then((data)=>{
+          if(!data) {
+            console.log("board is not found");
+          }
+          else {
+            let thread=data.threads.id(thread_id);
+            let reportedReply=thread.replies.id(reply_id);
+            reportedReply.reported=true;
+            reportedReply.bumped_on=new Date();
+            data.save().then((data)=>{
+                if(!data) {
+                  console.log("error updating in database");
+                }
+                else {
+                  res.send("reported");
+                }
+              });
+            }  
+        });
+      }).delete((req,res)=>{
+        const{thread_id,reply_id,delete_password}=req.body;
+        let board=req.body.board;
+        if(!board) {
+          board=req.params.board;
+        }
+        BoardModel.findOne({name:board}).then((data)=>{
+          if(!data) {
+            console.log("board is not found");
+          }
+          else {
+            let thread=data.threads.id(thread_id);
+            let replyToDelete=thread.replies.id(reply_id);
+            if(replyToDelete.delete_password===delete_password) {
+              replyToDelete.text='deleted';
+              replyToDelete.deleteOne();
+              data.save().then((data)=>{
+                if(!data) {
+                  console.log("error updating in database");
+                }
+                else {
+                  res.send("success");
+                }
+              });
+            }
+            else {
+              res.send("incorrect password");
+            }
+          }
+        });
+      })
 }
